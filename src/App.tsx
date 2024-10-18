c7c75d53d07cfcf2ac3818378e36badfd4b85908
pp.navillera <pp.navillera@gmail.com>
2024-10-19 00:18:01 +0900

    Design: configure UI based on Figma reference

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,87 @@
 import { useState } from "react";
-
+import cooker from "./assets/cooker.png";
+import email from "./assets/email.png";
+import pw from "./assets/password.png";
 function App() {
-  const [count, setCount] = useState(0);
+  const [id, setId] = useState();
+  const [password, setPassword] = useState();
+
+  const onSubmit = 1;
 
   return (
-    <>
-      <div></div>
-      <h1>ST:table</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+    <div className="flex flex-col justify-center items-center gap-10 mx-auto">
+      <img
+        src={cooker}
+        className="flex-grow-0 flex-shrink-0 w-[147px] h-[147px] object-cover"
+      />
+      <div
+        // href="https://your-homepage.com"
+        className="self-stretch flex-grow-0 flex-shrink-0 w-[500px] text-9xl text-center"
+      >
+        <span className="self-stretch flex-grow-0 flex-shrink-0 w-[500px] text-9xl text-left text-[#b90005] drop-shadow-2xl">
+          S
+        </span>
+        <span className="self-stretch flex-grow-0 flex-shrink-0 w-[500px] text-9xl text-left text-[#0a1e62] drop-shadow-2xl">
+          T
+        </span>
+        <span className="self-stretch flex-grow-0 flex-shrink-0 w-[500px] text-9xl text-left text-[#8f8f8f] drop-shadow-2xl">
+          :
+        </span>
+        <span className="self-stretch flex-grow-0 flex-shrink-0 w-[500px] text-9xl text-left text-gray-300 drop-shadow-md">
+          table
+        </span>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <form className="flex flex-col w-full gap-7" autoComplete="on">
+        <input
+          type="text"
+          placeholder="이메일"
+          style={{
+            backgroundImage: `url(${email})`,
+            backgroundSize: "35px",
+            backgroundPosition: "20px center",
+            backgroundRepeat: "no-repeat",
+          }}
+          name="email"
+          autoFocus
+          required
+          // className="flex-grow-0 flex-shrink-0 text-xl bg-[#e9e9e9]/60"
+          className="pl-16 text-xl flex items-center self-stretch flex-grow-0 flex-shrink-0 h-[76px] relative overflow-hidden rounded-[20px] bg-[#e9e9e9]/60"
+        />
+        <input
+          type="password"
+          placeholder="비밀번호"
+          style={{
+            backgroundImage: `url(${pw})`,
+            backgroundSize: "35px",
+            backgroundPosition: "20px center",
+            backgroundRepeat: "no-repeat",
+          }}
+          name="email"
+          autoFocus
+          required
+          // className="flex-grow-0 flex-shrink-0 text-xl bg-[#e9e9e9]/60"
+          className="pl-16 text-xl flex items-center self-stretch flex-grow-0 flex-shrink-0 h-[76px] relative overflow-hidden rounded-[20px] bg-[#e9e9e9]/60"
+        />
+
+        <div className="flex flex-col mt-4 gap-2">
+          <button
+            type="submit"
+            className="
+            self-stretch flex-grow-0 flex-shrink-0 relative overflow-hidden gap-2.5 px-[135px] py-5 rounded-[20px] bg-blue-500 text-white text-xl"
+          >
+            로그인
+          </button>
+          <button
+            type="button" // 기본 제출 동작을 하지 않도록 설정
+            // onClick={handleSignup}
+            className=" self-stretch flex-grow-0 flex-shrink-0 relative overflow-hidden gap-2.5 px-[135px] py-5 rounded-[20px] bg-blue-500 text-white text-xl"
+          >
+            회원가입
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
 
