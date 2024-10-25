0e66c70d49ba5ff8ad0ae8826e9ac8f49e4bbed0
pp.navillera <pp.navillera@gmail.com>
2024-10-26 04:03:44 +0900

    Design: fix and apply reponsive design
    Removed unnecessary code.

--- src/OrderItem.tsx
+++ src/OrderItem.tsx
@@ -8,17 +8,15 @@ interface Props {
 
 export function OrderItem({ time, quantity, menu, price }: Props) {
   return (
-    <div className="flex justify-start items-center    gap-[313px] p-5 border border-black">
-      <p className="  text-5xl font-bold text-left text-black">{time}</p>
-      <div className="flex flex-col justify-start items-start w-[373px]  gap-2.5">
-        <p className="  w-[450px] text-[40px] font-bold text-left text-black">
+    <div className="flex items-center justify-around gap-16 p-5 border-b border-black">
+      <p className="text-5xl font-bold text-left text-black">{time}</p>
+      <div className="flex flex-col justify-start items-start min-w-80 gap-2.5">
+        <p className="text-[40px] font-bold text-left text-black">
           [메뉴 {quantity}개] {price}원{" "}
         </p>
-        <p className="  w-[500px] text-[32px] font-bold text-left text-[#959595]">
-          {menu}
-        </p>
+        <p className="text-[32px] font-bold text-left text-[#959595]">{menu}</p>
       </div>
-      <div className="flex cursor-pointer justify-center items-center w-[170px] h-[170px] gap-2.5 mx-7 py-[15px] rounded-[10px] bg-[#b90005] border border-black text-[40px] font-bold text-center text-white">
+      <div className="flex cursor-pointer justify-center items-center w-40 h-40 gap-2.5 mx-7 py-[15px] rounded-[10px] bg-[#b90005] border border-black text-[40px] font-bold text-center text-white">
         접수하기
       </div>
     </div>
