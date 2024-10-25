4553886c9ca8cb4996d462d3f0f82054f7b5636b
pp.navillera <pp.navillera@gmail.com>
2024-10-26 02:29:18 +0900

    Design: adjust icon sizes and apply responsive design
    Removed unnecessary code.

--- src/Home/Menus.tsx
+++ src/Home/Menus.tsx
@@ -8,10 +8,10 @@ export default function Menus({ onClick, icon, title }: Props) {
   return (
     <div
       onClick={onClick}
-      className="cursor-pointer flex flex-col justify-center h-[353px] items-center relative"
+      className="cursor-pointer flex flex-col justify-center items-center"
     >
-      <img src={icon} className="w-40 h-40 object-cover" />
-      <p className="text-[40px] text-center text-black">{title}</p>
+      <img src={icon} className="w-28 h-32 lg:w-40 lg:h-40 " />
+      <p className="text-2xl lg:text-4xl text-center text-black">{title}</p>
     </div>
   );
 }
