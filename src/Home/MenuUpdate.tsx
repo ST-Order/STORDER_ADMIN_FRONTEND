50e82a4fdd12c79e63684f5d23ba1110d8f853bb
pp.navillera <pp.navillera@gmail.com>
2024-11-02 22:39:04 +0900

    Feat: make Menu Update page

--- src/Home/MenuUpdate.tsx
+++ src/Home/MenuUpdate.tsx
@@ -1,8 +1,31 @@
-import React from "react";
+import { MenuItem } from "./MenuItem";
 import Navbar from "./Navbar";
+import { useNavigate } from "react-router-dom";
 
 function MenuUpdate() {
-  return <Navbar>메뉴관리</Navbar>;
+  const nav = useNavigate();
+  return (
+    <Navbar>
+      <div className="flex flex-col items-center gap-[70px] h-full justify-evenly">
+        <div className="flex flex-col items-center self-stretch flex-grow-0 flex-shrink-0 gap-[34px]">
+          <div className="flex items-center text-[64px] font-bold text-black">
+            메뉴
+          </div>
+          <div className="flex items-start  flex-shrink-0 w-[633px] gap-11 rounded-[5px]">
+            <MenuItem title="삼겹덮밥" image="" />
+          </div>
+        </div>
+        <div
+          onClick={() => {
+            nav("/menu-update/register");
+          }}
+          className="flex cursor-pointer justify-center items-center w-[515px] h-[100px] px-[62px] py-[26px] rounded-[15px] bg-[#435fff] text-[40px] font-bold text-white"
+        >
+          메뉴 등록하기
+        </div>
+      </div>
+    </Navbar>
+  );
 }
 
 export default MenuUpdate;
---
+++ src/Home/XIcon.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+export default function XIcon() {
+  return (
+    <svg
+      width={48}
+      height={48}
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="cursor-pointer w-12 h-12 absolute left-[130px] top-0"
+      preserveAspectRatio="xMidYMid meet"
+    >
+      <path
+        d="M12 36L36 12M12 12L36 36"
+        stroke="black"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
